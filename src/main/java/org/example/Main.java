5ab80dafb39b6ce46694e8f9dd6c27299a70414e
danyazero <formulamgo2@gmail.com>
2024-05-18 13:51:46 +0300

    prettifying

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,9 @@ public class Main{
     private static final double INITIAL_STEP_SIZE = 1.0;
     private static final double MIN_STEP_SIZE = 0.25;
     private static final int MAX_ITERATIONS = 10000;
+    private static final String WARNING_COLOR = "\u001B[33m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RESET = "\u001B[0m";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -17,7 +20,7 @@ public class Main{
         System.out.print("Enter the value for c: ");
         double c = scanner.nextDouble();
 
-        System.out.println(a+"x^2 + "+b+"xy + "+c+"y^2 = "+f(a, b, c, 2, 3));
+//        System.out.println(a+"x^2 + "+b+"xy + "+c+"y^2 = "+f(a, b, c, 2, 3, true));
 
         double[] solution = coordinateDescent(a, b, c, 2, 3);
         System.out.println("Minimum found at: x = " + solution[0] + ", y = " + solution[1]);
@@ -29,26 +32,28 @@ public class Main{
         double stepSize = INITIAL_STEP_SIZE;
 
         for (int iteration = 1; iteration <= MAX_ITERATIONS; iteration++) {
-
-            double fPrev = f(a, b, c, x, y);
-
+            System.out.println();
             System.out.println("Iteration " + iteration + ":");
-            System.out.println("Starting values: x = " + x + ", y = " + y + ", f(x, y) = " + fPrev);
+
+            double fPrev = f(a, b, c, x, y, true);
+
+//            System.out.println("Starting values: x = " + x + ", y = " + y + ", f(x, y) = " + fPrev);
 
             // Step 1: Adjust x
             while (true) {
-                double fNext = f(a, b, c, x + stepSize, y);
-                printComparison(x, x + stepSize, y, fPrev, fNext);
+                double fNext = f(a, b, c, x + stepSize, y, true);
+                printComparison(new Point(x, y), new Point(x + stepSize, y), fPrev, fNext);
                 if (fNext < fPrev) {
                     x += stepSize;
                     fPrev = fNext;
                 } else {
-                    double fNextDecrease = f(a, b, c, x - stepSize, y);
+                    double fNextDecrease = f(a, b, c, x - stepSize, y, true);
                     if (fNextDecrease < fPrev) {
                         x -= stepSize;
                         fPrev = fNextDecrease;
                     } else {
-                        System.out.println("Variable x locked at: x = " + x + ", f(x, y) = " + f(a, b, c, x, y));
+                        System.out.println(WARNING_COLOR + "Variable x locked at: x = " + x + ", f(x, y) = " + f(a, b, c, x, y, false) + ANSI_RESET);
+                        System.out.println();
                         break;
                     }
                 }
@@ -56,18 +61,19 @@ public class Main{
 
             // Step 2: Adjust y
             while (true) {
-                double fNext = f(a, b, c, x, y + stepSize);
-                printComparison(y, x, y + stepSize, fPrev, fNext);
+                double fNext = f(a, b, c, x, y + stepSize, true);
+                printComparison(new Point(x, y), new Point(x, y+stepSize), fPrev, fNext);
                 if (fNext < fPrev) {
                     y += stepSize;
                     fPrev = fNext;
                 } else {
-                    double fNextDecrease = f(a, b, c, x, y - stepSize);
+                    double fNextDecrease = f(a, b, c, x, y - stepSize, true);
                     if (fNextDecrease < fPrev) {
                         y -= stepSize;
                         fPrev = fNextDecrease;
                     } else {
-                        System.out.println("Variable y fixed at: y = " + y + ", f(x, y) = " + f(a, b, c, x, y));
+                        System.out.println(WARNING_COLOR + "Variable y fixed at: y = " + y + ", f(x, y) = " + f(a, b, c, x, y, false) + ANSI_RESET);
+                        System.out.println();
                         break;
                     }
                 }
@@ -87,11 +93,16 @@ public class Main{
         return new double[]{x, y};
     }
 
-    private static double f(double a, double b, double c, double x, double y) {
-        return a * x * x + b * x * y + c * y * y;
+    private static double f(double a, double b, double c, double x, double y, boolean show) {
+        var firstPart =  a * x * x;
+        var secondPart = b * x * y;
+        var thirdPart =  c * y * y;
+        var result = firstPart + secondPart + thirdPart;
+        if (show) System.out.println("F(" + x + ", " + y + ") = " + firstPart + " + " + secondPart + " + " + thirdPart + " = " + result);
+        return result;
     }
 
-    private static void printComparison(double var1, double var2, double var3, double fPrev, double fNext) {
+    private static void printComparison(Point point, Point point2, double fPrev, double fNext) {
         String comparison;
         if (fPrev < fNext) {
             comparison = ">";
@@ -100,6 +111,7 @@ public class Main{
         } else {
             comparison = "=";
         }
-        System.out.println("Comparing f(x = " + var2 + ", y = " + var3 + "): " + fNext + " " + comparison + " " + fPrev);
+        System.out.println(ANSI_GREEN + "Comparing: " + fNext + " (" + point2.x() + ", " + point2.y() + ") " + comparison + " " + fPrev+ " (" + point.x() + ", " + point.y() + ")" + ANSI_RESET);
+        System.out.println();
     }
 }
---
+++ src/main/java/org/example/Point.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public record Point(Double x, Double y) {
+}
